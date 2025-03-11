c3f77f8a2a76bf6c134195d4237f097746957a34
Viccat57 <viccat75@gmail.com>
2025-03-11 12:37:57 -0600

    icono de la app añadido

--- resources/views/xd.blade.php
+++ resources/views/xd.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ZAIKOL</title>
+    <title>ZAIKO</title>
+    <link rel="icon" type="image/png" href="{{ asset('storage/logoZaiko.png') }}">
     <style>
         .dibujo {
             font-family: monospace;
